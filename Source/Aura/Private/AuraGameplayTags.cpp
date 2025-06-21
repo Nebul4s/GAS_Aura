6b19e25aa0029ea9f933dae37f67814711eeb73e
Nebul4s <j.hiltula24@gmail.com>
2025-06-21 13:02:34 +0300

    switching to native gameplay tags

---
+++ Source/Aura/Private/AuraGameplayTags.cpp
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AuraGameplayTags.h"
+#include "GameplayTagsManager.h"
+
+FAuraGameplayTags FAuraGameplayTags::GameplayTags;
+
+void FAuraGameplayTags::InitializeNativeGameplayTags()
+{
+	GameplayTags.Attributes_Primary_Strength = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Attributes.Primary.Strength"));
+	GameplayTags.Attributes_Primary_Intelligence = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Attributes.Primary.Intelligence"));
+	GameplayTags.Attributes_Primary_Resilience = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Attributes.Primary.Resilience"));
+	GameplayTags.Attributes_Primary_Dexterity = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Attributes.Primary.Dexterity"));
+	
+	GameplayTags.Attributes_Secondary_Armor = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Attributes.Secondary.Armor"));
+	GameplayTags.Attributes_Secondary_MaxHealth = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Attributes.Secondary.MaxHealth"));
+	GameplayTags.Attributes_Secondary_MaxMana = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Attributes.Secondary.MaxMana"));
+	GameplayTags.Attributes_Secondary_BlockChance = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Attributes.Secondary.BlockChance"));
+	GameplayTags.Attributes_Secondary_CriticalHitChance = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Attributes.Secondary.CriticalHitChance"));
+	GameplayTags.Attributes_Secondary_CriticalHitMultiplier = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Attributes.Secondary.CriticalHitMultiplier"));
+	GameplayTags.Attributes_Secondary_HealthRegeneration = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Attributes.Secondary.HealthRegeneration"));
+	GameplayTags.Attributes_Secondary_ManaRegeneration = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Attributes.Secondary.ManaRegeneration"));
+	GameplayTags.Attributes_Secondary_MovementSpeed = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Attributes.Secondary.MovementSpeed"));
+}
---
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+
+/**
+ * AuraGameplayTags
+ *
+ * Singleton containing native gameplay tags
+ */
+
+struct FAuraGameplayTags
+{
+	public:
+		static const FAuraGameplayTags& Get() { return GameplayTags; };
+		static void InitializeNativeGameplayTags();
+
+		FGameplayTag Attributes_Primary_Strength;
+		FGameplayTag Attributes_Primary_Intelligence;
+		FGameplayTag Attributes_Primary_Resilience;
+		FGameplayTag Attributes_Primary_Dexterity;
+	
+		FGameplayTag Attributes_Secondary_Armor;
+		FGameplayTag Attributes_Secondary_BlockChance;
+		FGameplayTag Attributes_Secondary_CriticalHitChance;
+		FGameplayTag Attributes_Secondary_CriticalHitMultiplier;
+		FGameplayTag Attributes_Secondary_HealthRegeneration;
+		FGameplayTag Attributes_Secondary_ManaRegeneration;
+		FGameplayTag Attributes_Secondary_MovementSpeed;
+		FGameplayTag Attributes_Secondary_MaxHealth;
+		FGameplayTag Attributes_Secondary_MaxMana;
+	protected:
+
+	private:
+		static FAuraGameplayTags GameplayTags;
+};
